a9f3edae0e4d78918c588de76d5f33c969e89220
frailyng <frailindes@gmail.com>
2025-05-28 19:23:23 -0400

    cambiando string a version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,33 +50,33 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     //room
-    implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.junit.ktx)
     implementation(libs.androidx.activity.ktx)
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
+    annotationProcessor(libs.room.compiler)
+    ksp(libs.room.compiler)
 
 
-    implementation("androidx.compose.foundation:foundation:1.5.0")
+    implementation(libs.androidx.foundation)
 
     //optional
-    implementation("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
-    implementation ("androidx.compose.material:material:1.4.0")
-    implementation("androidx.compose.material:material:1.3.1") // Usa la última versión
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.material3)
+    implementation (libs.androidx.material.v140)
+    implementation(libs.material) // Usa la última versión
 
 
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.51")
-    ksp("com.google.dagger:hilt-android-compiler:2.51")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     //Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.converter.moshi)
+    implementation(libs.logging.interceptor)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
