ec33e1579b730f66e89036d9b4cd786643b72ea3
frailyng <frailindes@gmail.com>
2025-05-28 21:13:18 -0400

    Agregando Funcionalidades Principales

--- app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/HomeNavHost.kt
+++
@@ -1,2 +0,0 @@
-package edu.ucne.aplicada2.parcial1.presentation.navigation
-
---
+++ app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/HostNavigation.kt
@@ -0,0 +1,9 @@
+package edu.ucne.aplicada2.parcial1.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HostNavigation(
+    navController: NavController,
+){}
