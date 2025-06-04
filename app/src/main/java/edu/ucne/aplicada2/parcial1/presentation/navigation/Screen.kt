535343aab65e4c96ba7ae705c7af77cd64d9bd79
frailyng <frailindes@gmail.com>
2025-06-04 18:47:58 -0400

    Screen Modificado con data object TareaList, y data class

--- app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/Screen.kt
@@ -1,4 +1,13 @@
 package edu.ucne.aplicada2.parcial1.presentation.navigation
 
-class Screen {
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+
+    @Serializable
+    data object TareaList: Screen()
+    @Serializable
+    data class TareaEntity(val tareaId: Int?) : Screen()
+    @Serializable
+    data object Home : Screen()
 }
