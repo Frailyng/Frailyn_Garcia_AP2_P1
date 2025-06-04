d7af6924ba86777d749385cbefa0565b5d4ccb6e
frailyng <frailindes@gmail.com>
2025-06-04 19:27:41 -0400

    HostNavigation Modificado

--- app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/HostNavigation.kt
+++ app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/navigation/HostNavigation.kt
@@ -1,9 +1,48 @@
 package edu.ucne.aplicada2.parcial1.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.aplicada2.parcial1.presentation.Home.HomeScreen
+import edu.ucne.aplicada2.parcial1.presentation.tareas.TareaListScreen
+import edu.ucne.aplicada2.parcial1.presentation.tareas.TareaScreen
+
 
 @Composable
 fun HostNavigation(
-    navController: NavController,
-){}
+    navHostController: NavHostController
+){
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.Home
+    ) {
+        //inicio
+        composable <Screen.Home> {
+            HomeScreen(navController = navHostController)
+        }
+
+        composable<Screen.TareaList>{
+
+            TareaListScreen(
+                goToTarea = { id ->
+                    navHostController.navigate(Screen.TareaEntity(id ?: 0))
+                },
+                createTarea = {
+                    navHostController.navigate((Screen.TareaEntity(0)))
+                }
+            )
+        }
+
+        composable <Screen.TareaEntity>{ backStack ->
+            val tareaId = backStack.toRoute<Screen.TareaEntity>().tareaId
+            TareaScreen(
+                tareaId = tareaId,
+                goBack = { navHostController.popBackStack() }
+            )
+        }
+    }
+}
+
+
