88b83528d170dd3c36f4ce65c06fc10e26f52757
frailyng <frailindes@gmail.com>
2025-06-04 19:33:54 -0400

    App Module Modificado

--- app/src/main/java/edu/ucne/aplicada2/parcial1/di/AppModule.kt
+++ app/src/main/java/edu/ucne/aplicada2/parcial1/di/AppModule.kt
@@ -1,4 +1,29 @@
 package edu.ucne.aplicada2.parcial1.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.aplicada2.parcial1.data.local.database.TareaDb
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideTareaDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            TareaDb::class.java,
+            "Tarea.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+
+    @Provides
+    fun provideTareaDao(tareaDb: TareaDb) = tareaDb.TareaDao()
 }
