a039a141e8bafa76e0cbb687fce522183ca009cc
frailyng <frailindes@gmail.com>
2025-06-04 18:58:09 -0400

    TareaUiState Modificado con sus entidades

--- app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/tareas/TareaUiState.kt
+++ app/src/main/java/edu/ucne/aplicada2/parcial1/presentation/tareas/TareaUiState.kt
@@ -1,4 +1,11 @@
 package edu.ucne.aplicada2.parcial1.presentation.tareas
 
-class TareaUiState {
-}
+import edu.ucne.aplicada2.parcial1.data.local.entities.TareaEntity
+
+data class TareaUiState(
+    val tareaId: Int? = null,
+    val descripcion: String = "",
+    val tiempo: Int = 0,
+    val errorMessage: String? = null,
+    val tareas: List<TareaEntity> = emptyList()
+)
